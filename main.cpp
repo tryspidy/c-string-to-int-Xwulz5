e3a98166365985ef0f7dbaf08beceb75cb951f1c
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-06 23:22:13 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// EXAMPLE
+std::string sStringAsString = "789";
+int iStringAsInt = atoi( sStringAsString.c_str() );
+
+/* SYNTAX
+atoi( <your-string>.c_str() )
+*/
+
+/* HEADERS
+#include <cstring>
+#include <string>
+*/
